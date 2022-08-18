ab3090dfad20ea51ea177c9a851114ade568d897
Greg Garcia <greg@garcia.casa>
2022-08-18 08:45:04 -0400

    Add iconography to tooltip PCF

---
+++ TooltipPCF/Components/Component.Types.ts
@@ -0,0 +1,10 @@
+export interface TooltipProps {
+  text?: string;
+  content?: string;
+  fontSize?: number;
+  height?: number;
+  width?: number;
+  margin?: string;
+  iconName?: string;
+  iconColor?: string;
+}
